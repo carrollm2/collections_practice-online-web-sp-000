d9d61e20a27d820fd82d7ec8a9c6bd3b7c3283cb
Michael Carroll <carrollm2@gmail.com>
2019-10-20 00:11:25 +0000

    Automatically backed up by Learn

--- spec/collections_practice_spec.rb
+++ spec/collections_practice_spec.rb
@@ -29,11 +29,11 @@ describe 'collections practice' do
       expect(swap_elements(["blake", "ashley", "scott"])).to eq(["blake", "scott", "ashley"])
     end
   end
-
+x
   # Bonus Question
   describe '#swap_elements_from_to' do
     it 'swap element with destination index' do
-      expect(swap_elements(["a", "b", "c"], 0, 2)).to eq(["c", "b", "a"])
+      expect(swap_elements_from_to(["a", "b", "c"], 0, 2)).to eq(["c", "b", "a"])
       #expect(swap_elements(["a", "b", "c"], 2, 1)).to eq(["a", "c", "b"])
     end
   end
